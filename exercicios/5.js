0cfba94b410150b6c85249277eb4758ac17a214a
Edu Melgaço <eduhdm0105@gmail.com>
2022-03-11 17:46:45 -0300

    add exercicio

---
+++ exercicios/5.js
@@ -0,0 +1,6 @@
+// Dado uma lista de numeros
+// printar cruzeiro, se ele for divisivel por 3
+// printar cabuloso, se ele for divisivel por 5
+// e se ele for divisivel por 3 e por 5, printar, galo doido
+
+var lista = [3, 5, 9, 12, 15, 18, 21, 30, 150];
