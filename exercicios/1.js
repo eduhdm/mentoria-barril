ae891971fcb939f0a73850c777d9219477994084
Eduardo Melgaço <eduardo.melgaco@paradigm.co>
2022-03-17 22:00:59 -0300

    Loki e lokis

--- exercicios/1.js
+++ exercicios/1.js
@@ -1,2 +1,13 @@
 // Definir variaveis n= 50 e m= 10
+var n = 50;
+var m = 10;
 // printar o resultado da adição, subtração, divisao, multiplicacao
+var adição = n + m;
+console.log("O resultado da SOMA entre essas duas váriavéis é:", adição);
+var subtração = n - m;
+console.log("O resultado da SUBTRAÇÃO entre essas duas váriavéis é:", subtração);
+var multiplicação = n * m;
+console.log("O resultado da MULTIPLICAÇÃO entre essas duas váriavéis é:", multiplicação);
+var divisão = n / m;
+console.log("O resultado da DIVISÂO entre essas duas váriavéis é:", divisão);
+
